f33ee2450da251e2e14842a710f1a03554c46b0e
JustAugucis <62718304+JustAugucis@users.noreply.github.com>
2023-11-20 11:20:09 +0100

    Structure

---
+++ Backend-Service/src/main/java/com/example/slaughterhousepart3/SlaughterHousePart3Application.java
@@ -0,0 +1,13 @@
+package com.example.slaughterhousepart3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlaughterHousePart3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlaughterHousePart3Application.class, args);
+	}
+
+}

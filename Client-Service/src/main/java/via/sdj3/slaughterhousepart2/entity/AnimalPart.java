82be0cc1c9e134cd0c9fa8156c3ae2b53fa41ea9
neptune55 <oskar.kabaliuk@protonmail.com>
2023-11-20 13:51:49 +0100

    Entities
    modified stuff

---
+++ Client-Service/src/main/java/via/sdj3/slaughterhousepart2/entity/AnimalPart.java
@@ -0,0 +1,41 @@
+package via.sdj3.slaughterhousepart2.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
+public class AnimalPart {
+    @Id
+    @Column
+    private int partId;
+    @Column
+    private String type;
+
+    public AnimalPart(int partId, String type) {
+        this.partId = partId;
+        this.type = type;
+    }
+
+    public AnimalPart() {
+
+    }
+
+    public int getPartId() {
+        return partId;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setPartId(int partId) {
+        this.partId = partId;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}

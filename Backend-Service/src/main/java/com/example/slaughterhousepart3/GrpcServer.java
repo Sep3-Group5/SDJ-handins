614eb3f02eda1c1b830b3f40b1f95c6fbbdca65e
JustAugucis <62718304+JustAugucis@users.noreply.github.com>
2023-11-20 16:26:51 +0100

    moved spring to the actual grp server

--- Backend-Service/src/main/java/com/example/slaughterhousepart3/GrpcServer.java
+++ Backend-Service/src/main/java/com/example/slaughterhousepart3/GrpcServer.java
@@ -7,6 +7,7 @@ import com.example.slaughterhousepart3.generated.animal.RequestText;
 import com.example.slaughterhousepart3.generated.animal.ResponseText;
 import com.example.slaughterhousepart3.repository.AnimalRepository;
 import com.example.slaughterhousepart3.server.AnimalServiceImplementation;
+import com.example.slaughterhousepart3.server.ProductPackageServiceImpl;
 import com.example.slaughterhousepart3.service.AnimalService;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
@@ -24,11 +25,54 @@ public class GrpcServer {
 		SpringApplication.run(GrpcServer.class, args);
 	}
 
-
+	/*
+	@Bean
 	public Server grpcServer(AnimalServiceImplementation animalServiceImplementation) throws IOException {
 		return ServerBuilder.forPort(8080)
 				.addService(animalServiceImplementation)
 				.build();
 	}
+*/
+
 
 }
+/*
+
+package com.example.slaughterhousepart3;
+
+		import com.example.slaughterhousepart3.generated.animal.AnimalObj;
+		import com.example.slaughterhousepart3.generated.product.ProductServiceGrpc;
+		import com.example.slaughterhousepart3.generated.animal.RequestText;
+		import com.example.slaughterhousepart3.generated.animal.ResponseText;
+		import io.grpc.Server;
+		import io.grpc.ServerBuilder;
+		import io.grpc.stub.StreamObserver;
+
+		import java.io.IOException;
+
+public class GrpcServer {
+	public static void main(String[] args) throws IOException, InterruptedException {
+		Server server = ServerBuilder.forPort(8080)
+
+				.addService(new ProductServiceGrpc.ProductServiceImplBase() {
+
+					public void addAnimal(AnimalObj request, StreamObserver<AnimalObj> responseObserver) {
+						super.ad
+						super.addAnimal(request, responseObserver);
+					}
+
+
+					public void getAnimalById(RequestText request, StreamObserver<ResponseText> responseObserver) {
+						super.getAnimalById(request, responseObserver);
+					}
+				}).build();
+
+		server.start();
+		System.out.println("Server started on port : " + server.getPort());
+		server.awaitTermination();
+
+	}
+}
+
+
+ */
--- Backend-Service/src/main/java/com/example/slaughterhousepart3/service/ProductService.java
+++ Backend-Service/src/main/java/com/example/slaughterhousepart3/service/ProductService.java
@@ -1,5 +1,7 @@
 package com.example.slaughterhousepart3.service;
 
+import com.example.slaughterhousepart3.entity.Animal;
+import com.example.slaughterhousepart3.entity.Product;
 import com.example.slaughterhousepart3.repository.AnimalPartRepository;
 import com.example.slaughterhousepart3.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +17,8 @@ public class ProductService {
         this.productRepository = productRepository;
     }
 
+    public void addProduct(Product product) {
+        productRepository.save(product);
+    }
 
-    // methods
 }

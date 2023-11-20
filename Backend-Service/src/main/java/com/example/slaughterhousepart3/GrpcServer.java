02144123bbbbd3cf7876709f37a43e229b415378
kubista9 <jakubkuka07@gmail.com>
2023-11-20 15:03:28 +0100

    server grpc done ulala habiby

---
+++ Backend-Service/src/main/java/com/example/slaughterhousepart3/GrpcServer.java
@@ -0,0 +1,34 @@
+package com.example.slaughterhousepart3;
+
+import com.example.slaughterhousepart3.generated.animal.AnimalObj;
+import com.example.slaughterhousepart3.generated.animal.ProductServiceGrpc;
+import com.example.slaughterhousepart3.generated.animal.RequestText;
+import com.example.slaughterhousepart3.generated.animal.ResponseText;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.stub.StreamObserver;
+
+import java.io.IOException;
+
+public class GrpcServer {
+	public static void main(String[] args) throws IOException, InterruptedException {
+		Server server = ServerBuilder.forPort(8080)
+
+			.addService(new ProductServiceGrpc.ProductServiceImplBase() {
+				@Override
+				public void addAnimal(AnimalObj request, StreamObserver<AnimalObj> responseObserver) {
+					super.addAnimal(request, responseObserver);
+				}
+
+				@Override
+				public void getAnimalById(RequestText request, StreamObserver<ResponseText> responseObserver) {
+					super.getAnimalById(request, responseObserver);
+				}
+			}).build();
+
+		server.start();
+		System.out.println("Server started on port : " + server.getPort());
+		server.awaitTermination();
+
+	}
+}
